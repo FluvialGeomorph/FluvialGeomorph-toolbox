a55028551623e5c4e38a575e3c4a0b2df6fd0891
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-03-30 09:25:05 -0500

    Switched buffer method to speed tool.

--- _07_DetrendDEM.py
+++ _07_DetrendDEM.py
@@ -1,14 +1,15 @@
 """____________________________________________________________________________
 Script Name:          _07_DetrendDEM.py
 Description:          Creates a detrended DEM. 
-Date:                 11/22/2017
+Date:                 01/14/2020
 
 Usage:
 This tool is based on the detrending method used in the River Bathymetry Toolkit (RBT) http://essa.com/tools/river-bathymetry-toolkit-rbt/. 
 
 Parameters:
-output_workspace (str)-- Path to the output workspace
-flowline_points  (str)-- Path to the flowline_points feature class.
+output_workspace (str)-- Path to the output workspace.
+flowline (str)        -- Path to the flowline feature class.
+flowline_points (str) -- Path to the flowline_points feature class.
 dem (str)             -- Path to the digital elevation model (DEM).
 buffer_distance (int) -- Distance the flowline_points feature class will be 
                          buffered to define the extent of the output 
@@ -16,14 +17,15 @@ buffer_distance (int) -- Distance the flowline_points feature class will be
                          system of the DEM. 
 
 Outputs:
-detrended              -- a new detrended DEM
+detrend               -- a new detrended DEM
 ____________________________________________________________________________"""
  
 import os
+from datetime import datetime
 import arcpy
 from arcpy.sa import *
 
-def DetrendDEM(output_workspace, flowline_points, dem, buffer_distance):
+def DetrendDEM(output_workspace, flowline, flowline_points, dem, buffer_distance):
     # Check out the extension license 
     arcpy.CheckOutExtension("3D")
     arcpy.CheckOutExtension("Spatial")
@@ -39,26 +41,31 @@ def DetrendDEM(output_workspace, flowline_points, dem, buffer_distance):
     
     # List parameter values
     arcpy.AddMessage("Workspace: {}".format(arcpy.env.workspace))
+    arcpy.AddMessage("Flowline: "
+                     "{}".format(arcpy.Describe(flowline).baseName))
     arcpy.AddMessage("Flowline Points: "
                      "{}".format(arcpy.Describe(flowline_points).baseName))
     arcpy.AddMessage("DEM: {}".format(arcpy.Describe(dem).baseName))
     arcpy.AddMessage("Buffer Distance: {}".format(str(buffer_distance)))
     
     # Buffer the flowline_points
+    #arcpy.AddMessage("Buffering flowline start: {}".format(datetime.now().strftime("%H:%M:%S")))
     flowline_buffer = os.path.join(output_workspace, "flowline_buffer")
-    arcpy.Buffer_analysis(in_features = flowline_points, 
+    arcpy.Buffer_analysis(in_features = flowline, 
                           out_feature_class = flowline_buffer, 
                           buffer_distance_or_field = buffer_distance, 
                           line_side = "FULL", 
                           line_end_type = "ROUND", 
                           dissolve_option = "ALL")
-    
-    arcpy.AddMessage("Buffered flowline_points")
+    arcpy.AddMessage("Buffering flowline: complete")
 
     # Set the environment mask to the flowline_buffer to clip all rasters
+    arcpy.AddMessage("Setting mask to flowline_buffer...")
     arcpy.env.mask = flowline_buffer
+    arcpy.AddMessage("Setting mask to flowline_buffer: complete")
     
     # Create the trend raster
+    arcpy.AddMessage("Creating trend raster...")
     trend = os.path.join(output_workspace, "trend")
     arcpy.Idw_3d(in_point_features = flowline_points, 
                  z_field = "Z", 
@@ -96,19 +103,20 @@ def DetrendDEM(output_workspace, flowline_points, dem, buffer_distance):
     
     # Cleanup
     arcpy.Delete_management(in_data = flowline_buffer)
-    arcpy.Delete_management(in_data = trend)
+    # arcpy.Delete_management(in_data = trend)
     arcpy.Delete_management(in_data = "trend_smooth")
         
 
 def main():
     # Call the DetrendDEM function with command line parameters
-    DetrendDEM(output_workspace, flowline_points, dem, buffer_distance)
+    DetrendDEM(output_workspace, flowline, flowline_points, dem, buffer_distance)
 
 if __name__ == "__main__":
     # Get input parameters
     output_workspace = arcpy.GetParameterAsText(0)
-    flowline_points  = arcpy.GetParameterAsText(1)
-    dem              = arcpy.GetParameterAsText(2)
-    buffer_distance  = arcpy.GetParameterAsText(3)
+    flowline         = arcpy.GetParameterAsText(1)
+    flowline_points  = arcpy.GetParameterAsText(2)
+    dem              = arcpy.GetParameterAsText(3)
+    buffer_distance  = arcpy.GetParameterAsText(4)
     
     main()
