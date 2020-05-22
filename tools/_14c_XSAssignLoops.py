b0f4ce213084702e545eb3bab8973265749bacb0
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-05-22 11:47:39 -0500

    Added explicit data references for Pro compatability.

--- tools/_14c_XSAssignLoops.py
+++ tools/_14c_XSAssignLoops.py
@@ -18,9 +18,9 @@ bankline_points       -- Path to the bankline_points feature class.
 Outputs:
 Updates the cross_section feature class with new fields for loop and bend. 
 ____________________________________________________________________________"""
- 
+
+import os 
 import arcpy
-import os
 
 def XSAssignLoops(output_workspace, cross_section, bankline_points):
     # Set environment variables 
@@ -47,24 +47,25 @@ def XSAssignLoops(output_workspace, cross_section, bankline_points):
                             where_clause = "loop IS NOT NULL")
     
     # Spatial Join bankline_points with the closest (within 5m) loop_point
+    xs_fc = os.path.join(output_workspace, "xs_fc")
     arcpy.SpatialJoin_analysis(target_features = cross_section, 
                                join_features = "loop_bl_pts", 
-                               out_feature_class = "xs_fc",  
+                               out_feature_class = xs_fc,  
                                match_option = "CLOSEST",
                                search_radius = 5)
                                
     # Join `xs_fc.loop` and `bend` to the `cross_section` feature class
     arcpy.JoinField_management(in_data = cross_section,
                                in_field = "Seq",
-                               join_table = "xs_fc",
+                               join_table = xs_fc,
                                join_field = "Seq",
                                fields = ["loop", "bend"])
     
     # Return
-    arcpy.SetParameters(3, cross_section)
+    arcpy.SetParameter(3, cross_section)
     
     # Cleanup
-    arcpy.Delete_management("xs_fc")
+    arcpy.Delete_management(xs_fc)
 
 def main():
     XSAssignLoops(output_workspace, cross_section, bankline_points)
