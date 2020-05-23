e69ea40a8e96886fef67dad7971027e7b2bd3c55
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-05-23 11:33:24 -0500

    Added new functions to help troubleshoot tool input parameters

--- install/FG_utils.R
+++ install/FG_utils.R
@@ -132,6 +132,79 @@ set_pandoc <- function() {
     }
 }
 
+
+#' @title Parameter information
+#' 
+#' @description Calculates the characteristics of a parameter. 
+#' 
+#' @param param        character; The parameter.
+#' @param param_name   character; The name of the parameter. 
+#' 
+#' @return A single-row data frame containing the characteristics of a parameter.
+#' 
+param_info <- function(param, param_name) {
+    # Calculate param info fields
+    type <- typeof(param)
+    
+    # Convert into a data frame
+    param_row <- tibble(param_name, type)
+}
+
+#' @title Parameters information table
+#' 
+#' @description Calculates a table of parameter characteristics for a set of 
+#' parameters. 
+#' 
+#' @param param_list   list; A list of named parameter values.
+#' 
+#' @return A data frame of parameters and their characteristics. 
+#' 
+param_table <- function(param_list) {
+    # Get a list of parameter names
+    param_names <- as.list(names(param_list))
+    
+    # Get info on each parameter
+    param_info_table <- purrr::map2(.x = param_list, .f = param_info, 
+                                    .y = param_names)
+    
+    # Combine all parameters into a single data frame
+    info_table <- dplyr::bind_rows(param_info_table)
+}
+
+#' @title Compare tool parameters
+#' 
+#' @description Compares the parameters passed by ESRI to R. The table returned 
+#' helps to troubleshoot data types conversion between ESRI and R. 
+#' 
+#' @param in_params    list; List of parameter values created by an ESRI 
+#'                     script tool.
+#' @param param_list   list; List of parameter values in R. List items should 
+#'                     be named by the names of the parameters. 
+#'                     
+#' @return A data frame of parameters and their characteristics. 
+#' 
+compare_params <- function(in_params, param_list) {
+    # Set parameter names for in_params
+    param_names <- as.list(names(param_list))
+    in_params <- setNames(in_params, param_names)
+    
+    # Get parameters from ESRI and rename fields
+    params_esri <- param_table(in_params)
+    params_esri <- dplyr::rename(params_esri, esri_type = type)
+    
+    # Get parameters in R and rename fields
+    params_r <- param_table(param_list)
+    params_r <- dplyr::rename(params_r, r_type = type)
+    
+    # Build a table to comapre the two
+    param_table <- dplyr::left_join(params_esri, params_r,
+                                    by = "param_name")
+    # param_table <- merge(params_esri, params_r, 
+    #                      by = "param_name")
+}
+
+
+
 #' @title Convert Windows path to forward slashes
 #' 
 #' @description Convert a Windows file path from double backslashes to single 
