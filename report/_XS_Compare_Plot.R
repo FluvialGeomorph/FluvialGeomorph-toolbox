561c7900bfe65728312a0d8cc97a36cd088b9799
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-06-18 11:39:42 -0500

    updated XS_Compare_Plot

--- report/_XS_Compare_Plot.R
+++ report/_XS_Compare_Plot.R
@@ -62,29 +62,22 @@ tool_exec <- function(in_params, out_params) {
     # survey_name_4 <- NULL
 
     # Create list of survey paths
-    print("Assembling survey events")
     xs_points_paths <- list(xs_points_1, xs_points_2, xs_points_3, xs_points_4)
-    #xs_points_paths <- dplyr::na_if(xs_points_paths, "")
     
     # Name the survey paths list by their survey names
     survey_names <- c(survey_name_1, survey_name_2, survey_name_3, survey_name_4)
-    #survey_names <- dplyr::na_if(survey_names, "")
     xs_points_paths <- setNames(xs_points_paths, survey_names)
-    print(xs_points_paths)
     
     # Eliminate empty surveys
-    print("Discarding empty items")
     xs_points_paths <- purrr::discard(xs_points_paths, is.null)
     
-    # Convert list of survey paths to list of sp objects
-    print("Converting to sp")
-    xs_pts_sp <- purrr::map(xs_points_paths, fluvgeo::arc2sp)
+    # Convert list of survey paths to list of sf objects
+    xs_pts_sf_list <- purrr::map(xs_points_paths, fluvgeo::fc2sf)
     
     # Call the graph function
-    print("Calling plot")
     print(fluvgeo::xs_compare_plot(stream = stream,
                                    xs_number = xs_number,
-                                   xs_pts_list = xs_pts_sp))
+                                   xs_pts_sf_list = xs_pts_sf_list))
     
     return(out_params)
 }
