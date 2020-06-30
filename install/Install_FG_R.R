7f9ce31ec07e5290079e44ab3ad693aa2f06d096
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-06-30 15:24:32 -0500

    call the improved `load_fluvgeo_packages` function

--- install/Install_FG_R.R
+++ install/Install_FG_R.R
@@ -13,18 +13,12 @@ tool_exec <- function(in_params, out_params) {
     dir_name <- getSrcDirectory(function(x) {x})
     source(file.path(dir_name, "FG_utils.R"))
     message(paste(objects()))
-    #message(as.character(dirname))
     
     # gp tool parameters
     reinstall <- in_params[[1]]
     
-    # Uninstall FluvialGeomorph R packages
-    if (reinstall == TRUE) {
-        uninstall_fluvgeo_packages()
-    }
-    
     # Load FluvialGeomorph R packages
-    load_fluvgeo_packages()
+    load_fluvgeo_packages(force = reinstall)
     
     # Set pandoc
     set_pandoc()
