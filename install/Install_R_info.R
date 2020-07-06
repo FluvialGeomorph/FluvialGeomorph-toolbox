cd28edf617a36fa475daf785201b5de9fbaaa7a0
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-07-06 08:04:38 -0500

    removed devtools because of circular install issue

--- install/Install_R_info.R
+++ install/Install_R_info.R
@@ -7,26 +7,15 @@
 #' @return Reports R installation information. 
 #'
 tool_exec <- function(in_params, out_params) {
-    # Get R session info
-    if("devtools" %in% installed.packages()[, "Package"]) {
-        print(devtools::session_info())
-    } else {
-        print(sessionInfo())
-        
-        # Print R libsPath
-        message(paste("R libsPath: ", .libPaths(), collapse = "\n"))
-    }
+    print(sessionInfo())
+    # Print R libsPath
+    message(paste("R libsPath: ", .libPaths(), collapse = "\n"))
     
     # System environment variables
     print(Sys.getenv())
     
     # Determine if pandoc is available
     print(paste("Pandoc available: ", rmarkdown::pandoc_available()))
-    
-    # Get R version
-    print("R version:")
-    print(R.Version())
-    print(Sys.info()[["machine"]])
     
     return(out_params)
 }
