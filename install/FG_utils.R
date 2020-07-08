7fea156b5b617921a296825070f3544fb90481f1
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-07-08 17:01:20 -0500

    removed foward_slash function

--- install/FG_utils.R
+++ install/FG_utils.R
@@ -168,16 +168,3 @@ compare_params <- function(in_params, param_list) {
     # param_table <- merge(params_esri, params_r, 
     #                      by = "param_name")
 }
-
-
-#' @title Convert Windows path to forward slashes
-#' 
-#' @description Convert a Windows file path from double backslashes to single 
-#' forward slash file seperators suitable for use in R. 
-#' 
-#' @param path        character; A windows file path containing escaped 
-#'                    backslashs (i.e., \\)
-#' 
-forward_slash <- function(path) {
-    path <- gsub("\\\\", "/", path)
-}
