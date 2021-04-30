ca8baedc01a87b140cd8fa424dabe1d974d03662
Michael Dougherty <mike.p.doc.71@gmail.com>
2021-04-30 12:40:48 -0500

    added documentation

--- install/session.R
+++ install/session.R
@@ -1,3 +1,11 @@
+#' @title R Session Info
+#' 
+#' @description Reports the R session info. 
+#' 
+#' @export
+#' 
+#' @return None.
+#'  
 tool_exec <- function(in_params, out_params) {
     require(fluvgeo)
     print(sessionInfo())
