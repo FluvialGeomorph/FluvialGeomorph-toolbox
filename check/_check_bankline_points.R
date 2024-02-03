3f911c7e07ccddfc9270adb529505c1d57edeacb
Michael <mike.p.doc.71@gmail.com>
2024-02-03 09:30:50 -0600

    updated the check_bankline_points tool

--- check/_check_bankline_points.R
+++ check/_check_bankline_points.R
@@ -1,27 +1,38 @@
-#' Checks the `bankline_points` feature class.
+#' @title Check the validity of a `fluvgeo` `bankline_points` data structure
 #'
-#' Args:
-#'    bankline_points_fc  character; the full path to an ESRI bankline_points 
-#'                        feature class
+#' @description Checks that the input data structure `bankline_points` meets
+#' the requirements for this data structure.
+#'
+#' @export
+#' @param bankline_points_fc  character; a `bankline_points` feature class
+#'                            used by the fluvgeo package.
+#' 
+#' @details This is a wrapper to the `fluvgeo::check_bankline_points` function.
+#' 
+#' @return Returns TRUE if the `bankline_points` data structure matches the
+#' requirements. The function throws an error for a data structure not matching
+#' the data specification. Returns errors describing how the the data structure
+#' doesn't match the requirement.
 #'
 tool_exec <- function(in_params, out_params) {
+    # Declare location of script within the toolbox
+    here::i_am("check/_check_bankline_points.R")
     # Load utility R functions
-    dir_name <- getSrcDirectory(function(x) {x})
-    fg <- dirname(dir_name)
-    fg_install <- file.path(fg, "install")
-    source(file.path(fg_install, "FG_utils.R"))
-    load_packages(c("sp", "dplyr", "raster", "fluvgeo"))
+    fg_utils <- here::here("install", "FG_utils.R")
+    source(fg_utils)
+    message("Sourced utility functions: ", fg_utils)
+    load_packages(c("dplyr", "raster", "fluvgeo"))
     
     # gp tool parameters
     bankline_points_fc  <- in_params[[1]]
     
-    # Import fc to sp
-    bankline_points_sp <- arc2sp(bankline_points_fc)
+    # Import fc to sf
+    bankline_points_sf <- fc2sf(bankline_points_fc)
     
     fc_name <- basename(bankline_points_fc)
     
     # Check bankline_points and print messages
-    check <- try(check_bankline_points(bankline_points_sp))
+    check <- try(check_bankline_points(bankline_points_sf))
     if(inherits(check, "try-error")) {
         print(geterrmessage())
     } else { 
