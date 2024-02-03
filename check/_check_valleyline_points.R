85b9da0c2ca1eb75bab07016c5aa90f508acd1a5
Michael <mike.p.doc.71@gmail.com>
2024-02-03 12:51:59 -0600

    updated the check_valleyline_points tool

--- check/_check_valleyline_points.R
+++ check/_check_valleyline_points.R
@@ -1,28 +1,36 @@
-#' Checks the `valleyline_points` feature class.
+#' @title Check the validity of an `fluvgeo` `valleyline_points` data structure
 #'
-#' Args:
-#'    valleyline_points_fc  character; the full path to an ESRI 
-#'                          valleyline_points feature class
+#' @description Checks that the input data structure `valleyline_points` meets
+#' the requirements for this data structure.
+#'
+#' @export
+#' @param valleyline_points_fc   character: a `valleyline_points` feature class
+#'                               data structure used by the fluvgeo package.
+#'
+#' @return Returns TRUE if the `valleyline_points` data structure matches the
+#' requirements. The function throws an error for a data structure not matching
+#' the data specification. Returns errors describing how the the data structure
+#' doesn't match the requirement.
 #'
 tool_exec <- function(in_params, out_params) {
+    # Declare location of script within the toolbox
+    here::i_am("check/_check_valleyline_points.R")
     # Load utility R functions
-    dir_name <- getSrcDirectory(function(x) {x})
-    fg <- dirname(dir_name)
-    fg_install <- file.path(fg, "install")
-    source(file.path(fg_install, "FG_utils.R"))
-    # Load required libraries
-    load_packages(c("sp", "dplyr", "raster", "fluvgeo"))
+    fg_utils <- here::here("install", "FG_utils.R")
+    source(fg_utils)
+    message("Sourced utility functions: ", fg_utils)
+    load_packages(c("dplyr", "raster", "fluvgeo"))
     
     # gp tool parameters
     valleyline_points_fc  <- in_params[[1]]
     
-    # Import fc to sp
-    valleyline_points_sp <- arc2sp(valleyline_points_fc)
+    # Import fc to sf
+    valleyline_points_sf <- fc2sf(valleyline_points_fc)
     
     fc_name <- basename(valleyline_points_fc)
     
     # Check valleyline_points and print messages
-    check <- try(check_valleyline_points(valleyline_points_sp))
+    check <- try(check_valleyline_points(valleyline_points_sf))
     if(inherits(check, "try-error")) {
         print(geterrmessage())
     } else { 
